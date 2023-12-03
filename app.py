e66fefae8a9f0fc33780e4a5a78a59da9b409bb1
Julian Gabriel <148124142+muskmanelon@users.noreply.github.com>
2023-12-03 12:28:57 +0100

    s

--- app.py
+++ app.py
@@ -40,4 +40,5 @@ def get_best_move_api():
         return jsonify({'error': 'Internal Server Error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
